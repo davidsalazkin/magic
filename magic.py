7b3626e56a5bccf50b0ce622ad034b1f175aa37b
David Salazkin <david.salazkin@gmail.com>
2019-10-30 14:30:45 -0500

    added Google analytics to documentation; updated documentation

--- magic.py
+++ magic.py
@@ -1813,6 +1813,7 @@ class Interpreter:
 #######################################
 
 global_symbol_table = SymbolTable()
+# Constants
 global_symbol_table.set("ПУСТОЙ", Number.null)
 global_symbol_table.set("ИСТИНА", Number.true)
 global_symbol_table.set("ЛОЖЬ", Number.false)
@@ -1820,6 +1821,7 @@ global_symbol_table.set("Е", Number.math_E)
 global_symbol_table.set("ПИ", Number.math_PI)
 global_symbol_table.set("ТАУ", Number.math_TAU)
 global_symbol_table.set("БЕСКОНЕЧНОСТЬ", Number.math_INF)
+# General Functions
 global_symbol_table.set("СООБЩИТЬ", BuiltInFunction.print)
 global_symbol_table.set("СТРОКА", BuiltInFunction.string)
 global_symbol_table.set("ЦЕЛОЕ", BuiltInFunction.int)
@@ -1830,11 +1832,13 @@ global_symbol_table.set("ЕСТЬ_ЧИСЛО", BuiltInFunction.is_number)
 global_symbol_table.set("ЕСТЬ_СТРОКА", BuiltInFunction.is_string)
 global_symbol_table.set("ЕСТЬ_СПИСОК", BuiltInFunction.is_list)
 global_symbol_table.set("ЕСТЬ_ФУНКЦИЯ", BuiltInFunction.is_function)
+# List Functions
 global_symbol_table.set("ДОБАВИТЬ", BuiltInFunction.append)
 global_symbol_table.set("ИЗВЛЕКАТЬ", BuiltInFunction.pop)
 global_symbol_table.set("ОБЬЕДИНИТЬ", BuiltInFunction.extend)
 global_symbol_table.set("ДЛИНА", BuiltInFunction.length)
 global_symbol_table.set("СОРТ", BuiltInFunction.sorted)
+# Math Functions
 global_symbol_table.set("КВАД_КОР", BuiltInFunction.sqrt)
 global_symbol_table.set("ЭКСПОНЕНТА", BuiltInFunction.pow)
 global_symbol_table.set("АБСОЛЮТНАЯ", BuiltInFunction.abs)
